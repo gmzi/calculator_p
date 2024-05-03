1289404a56f7544ef7f85573952ab433f33d1507
gmzi <gmzieres@gmail.com>
2024-05-03 11:14:20 -0400

    intercept fetch test

--- public/sw.js
+++ public/sw.js
@@ -162,24 +162,30 @@ self.addEventListener('activate', async (event) => {
 });
 
 // Fetch event listener: Serve from cache if offline
-// self.addEventListener('fetch', (event) => {
-//     event.respondWith(async function () {
-//         const cachedResponse = await caches.match(event.request);
+self.addEventListener('fetch', (event) => {
+    return new Response('<h1>YOUR FETCH HAS BEEN INterCEPTED</h1>', {
+        headers: {
+            'Content-Type': 'text/html'
+        }
+    });
 
-//         if (cachedResponse) {
-//             return cachedResponse;
-//         }
+    event.respondWith(async function () {
+        const cachedResponse = await caches.match(event.request);
 
-//         // Fallback to network if not in cache and online
-//         if (navigator.onLine) {
-//             return fetch(event.request);
-//         }
+        if (cachedResponse) {
+            return cachedResponse;
+        }
 
-//         // Fallback to offline page if offline and not in cache
-//         return new Response('<h1>Content not available offline</h1>', {
-//             headers: {
-//                 'Content-Type': 'text/html'
-//             }
-//         });
-//     }());
-// });
+        // Fallback to network if not in cache and online
+        if (navigator.onLine) {
+            return fetch(event.request);
+        }
+
+        // Fallback to offline page if offline and not in cache
+        return new Response('<h1>Content not available offline</h1>', {
+            headers: {
+                'Content-Type': 'text/html'
+            }
+        });
+    }());
+});
