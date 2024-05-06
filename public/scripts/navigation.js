c23e28c49fb692bf5ec3829c102023f9e16e4b46
gmzi <gmzieres@gmail.com>
2024-05-06 13:38:00 -0400

    change navigation logic

--- public/scripts/navigation.js
+++ public/scripts/navigation.js
@@ -1,34 +1,40 @@
-const currentUrl = window.location.href;
-const BASE_URL = `${window.origin}/`;
+// const currentUrl = window.location.href;
+// const BASE_URL = `${window.origin}/`;
 
-console.log('origin: ')
-console.log(origin)
+// function extractUrlPart() {
+//     const origin = window.location.origin;
+//     const extensionIndex = currentUrl.indexOf(".")
+//     const startIndex = BASE_URL.length
+//     const endIndex = extensionIndex
+//     const urlPart = currentUrl.substring(startIndex, endIndex)
+//     return urlPart
+// }
 
-function extractUrlPart() {
-    const origin = window.location.origin;
-    const extensionIndex = currentUrl.indexOf(".")
-    const startIndex = BASE_URL.length
-    const endIndex = extensionIndex
-    const urlPart = currentUrl.substring(startIndex, endIndex)
-    return urlPart
-}
+// function makeActive() {
+//     let urlPart = extractUrlPart()
+//     const navLinks = document.querySelectorAll('nav ul li a')
 
-function makeActive() {
-    let urlPart = extractUrlPart()
-    const navLinks = document.querySelectorAll('nav ul li a')
+//     navLinks.forEach(function (link) {
+//         const attr = link.getAttribute('href')
+//         if (attr === '/' && urlPart === BASE_URL) {
+//             link.classList.add('active-page');
+//         } else if (attr.includes(urlPart)) {
+//             link.classList.add('active-page');
+//         } else {
+//             link.classList.remove('active-page');
+//         }
+//     })
+// }
 
-    navLinks.forEach(function (link) {
-        const attr = link.getAttribute('href')
-        if (attr === '/' && urlPart === BASE_URL) {
-            link.classList.add('active-page');
-        } else if (attr.includes(urlPart)) {
-            link.classList.add('active-page');
-        } else {
-            link.classList.remove('active-page');
-        }
-    })
-}
+// document.addEventListener('DOMContentLoaded', function () {
+//     makeActive();
+// });
 
-document.addEventListener('DOMContentLoaded', function () {
-    makeActive();
+const currentPath = window.location.pathname
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach(link => {
+    if (link.getAttribute('href') === currentPath) {
+        link.classList.add('active-page');
+    }
 });
